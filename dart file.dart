fed09b98bdb6dfcccfc6eea4b4be1d4b57e9c831
akinkunmidanny2 <35771028-akinkunmidanny2@users.noreply.replit.com>
2025-09-27 12:06:50 +0000

    Initial commit

---
+++ dart file.dart
@@ -0,0 +1,6 @@
+import "dart:io";
+
+void main{} {
+ print ("My name is lucky")
+
+}
---
+++ main.dart
@@ -0,0 +1,9 @@
+import "dart:io";
+import 'dart:math';
+
+void main() {
+ List <int> favNums =  [12, 21, 54, 89, 76];
+ favNums.add(76);
+print (favNums);
+  
+}
